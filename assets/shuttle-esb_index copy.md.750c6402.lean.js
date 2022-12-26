ce6a10923d9f2e374fa861117499d2bc9f72e860
Eben <me@ebenroux.co.za>
2022-12-26 16:24:30 +0200

    deploy

---
+++ assets/shuttle-esb_index copy.md.750c6402.lean.js
@@ -0,0 +1,43 @@
+import { r as resolveComponent, o as openBlock, c as createElementBlock, e as createVNode, a as createBaseVNode, b as createTextVNode } from "./app.837496c7.js";
+const _hoisted_1 = /* @__PURE__ */ createBaseVNode("h1", {
+  id: "test",
+  tabindex: "-1"
+}, [
+  /* @__PURE__ */ createTextVNode("Test "),
+  /* @__PURE__ */ createBaseVNode("a", {
+    class: "header-anchor",
+    href: "#test",
+    "aria-hidden": "true"
+  }, "#")
+], -1);
+const _hoisted_2 = /* @__PURE__ */ createBaseVNode("hr", null, null, -1);
+const _hoisted_3 = /* @__PURE__ */ createBaseVNode("h2", {
+  id: "page-truetitle-home",
+  tabindex: "-1"
+}, [
+  /* @__PURE__ */ createTextVNode("page: true title: Home "),
+  /* @__PURE__ */ createBaseVNode("a", {
+    class: "header-anchor",
+    href: "#page-truetitle-home",
+    "aria-hidden": "true"
+  }, "#")
+], -1);
+const __pageData = JSON.parse('{"title":"Test","description":"","frontmatter":{},"headers":[{"level":2,"title":"page: truetitle: Home","slug":"page-truetitle-home","link":"#page-truetitle-home","children":[]}],"relativePath":"shuttle-esb/index copy.md"}');
+const __default__ = { name: "shuttle-esb/index copy.md" };
+const _sfc_main = Object.assign(__default__, {
+  setup(__props) {
+    return (_ctx, _cache) => {
+      const _component_Home = resolveComponent("Home");
+      return openBlock(), createElementBlock("div", null, [
+        _hoisted_1,
+        _hoisted_2,
+        _hoisted_3,
+        createVNode(_component_Home)
+      ]);
+    };
+  }
+});
+export {
+  __pageData,
+  _sfc_main as default
+};
---
+++ assets/shuttle-recall_index.md.86142d91.js
@@ -0,0 +1,27 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, a as createBaseVNode, b as createTextVNode } from "./app.837496c7.js";
+const __pageData = JSON.parse('{"title":"Shuttle.Recall","description":"","frontmatter":{},"headers":[],"relativePath":"shuttle-recall/index.md"}');
+const _sfc_main = { name: "shuttle-recall/index.md" };
+const _hoisted_1 = /* @__PURE__ */ createBaseVNode("h1", {
+  id: "shuttle-recall",
+  tabindex: "-1"
+}, [
+  /* @__PURE__ */ createTextVNode("Shuttle.Recall "),
+  /* @__PURE__ */ createBaseVNode("a", {
+    class: "header-anchor",
+    href: "#shuttle-recall",
+    "aria-hidden": "true"
+  }, "#")
+], -1);
+const _hoisted_2 = /* @__PURE__ */ createBaseVNode("p", null, "An event sourcing/projection mechanism that abstracts the storage of events.", -1);
+const _hoisted_3 = [
+  _hoisted_1,
+  _hoisted_2
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_3);
+}
+const index = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  index as default
+};
